f7bf0c6e8845fad724a9bb152eaa042ed9f917e2
rocean <rocean74@gmail.com>
2020-02-25 17:07:10 +0200

    Edit script

--- checkNewPosts.sh
+++ checkNewPosts.sh
@@ -7,7 +7,7 @@
 
 # Edit with your settings
 URI="https://<ny.wordpress-site.com>/wp-json/wp/v2/posts"
-PROJECT_PATH="<full project path>"
+PROJECT_PATH='<full project path>'
 
 CSUM="/tmp/.checksum."`basename -s sh  $0`
 TMP=`mktemp`
@@ -15,7 +15,7 @@ TMP=`mktemp`
 curl -s "$URI" > $TMP
 [ -f $CSUM ] || md5sum $TMP | awk '{print $1}' > $CSUM
 md5sum $TMP  | awk '{print $1}' > "$TMP.md5"
-cd $PROJECT_PATH
+cd "$PROJECT_PATH"
 cmp --silent $CSUM "$TMP.md5" || ( `gridsome build 1>&2 /dev/null` && md5sum $TMP | awk '{print $1}' > $CSUM )
 rm -f "$TMP" "$TMP.md5"
 
